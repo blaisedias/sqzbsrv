998bc5d260ac3373d38deefd8271d3b47aed2ece
Blaise Dias <diasblaise@gmail.com>
2015-06-19 23:22:38 +0100

    Update scripts to supply type paramete to s_rtags executable

--- scripts/test3.sh
+++ scripts/test3.sh
@@ -5,9 +5,9 @@ bin/rtags scan /media/seagate500GB/music/collection/flacs.cds /media/seagate500G
 bin/rtags load test
 bin/rtags load dump > /tmp/dump.txt
 
-bin/s_rtags scan /media/seagate500GB/music/collection/flacs.cds /media/seagate500GB/music/collection/mp3s.only save
-bin/s_rtags load test
-bin/s_rtags load dump > /tmp/s_dump.txt
+bin/s_rtags type songsscan /media/seagate500GB/music/collection/flacs.cds /media/seagate500GB/music/collection/mp3s.only save
+bin/s_rtags type songsload test
+bin/s_rtags type songsload dump > /tmp/s_dump.txt
 
 diff txt/albums.txt  txt/s_albums.txt  
 diff txt/artists.txt  txt/s_artists.txt  
--- scripts/test3s.sh
+++ scripts/test3s.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-bin/s_rtags scan /media/seagate500GB/music/collection/flacs.cds /media/seagate500GB/music/collection/mp3s.only save
-bin/s_rtags load test
-bin/s_rtags load dump > /tmp/s_dump.txt
+bin/type songss_rtags scan /media/seagate500GB/music/collection/flacs.cds /media/seagate500GB/music/collection/mp3s.only save
+bin/type songss_rtags load test
+bin/type songss_rtags load dump > /tmp/s_dump.txt
 diff txt/albums.txt  txt/s_albums.txt  
 diff txt/artists.txt  txt/s_artists.txt  
 diff txt/genres.txt  txt/s_genres.txt  
