d0d8145fb7f919c73977e035a278f5cba3e0dc78
Blaise Dias <diasblaise@gmail.com>
2015-07-28 22:34:55 +0100

    test script update

--- scripts/test3.sh
+++ scripts/test3.sh
@@ -6,8 +6,8 @@ bin/rtags load test
 bin/rtags load dump > /tmp/dump.txt
 
 bin/s_rtags type songsscan /media/seagate500GB/music/collection/flacs.cds /media/seagate500GB/music/collection/mp3s.only save
-bin/s_rtags type songsload test
-bin/s_rtags type songsload dump > /tmp/s_dump.txt
+bin/s_rtags type songs load test
+bin/s_rtags type songs load dump > /tmp/s_dump.txt
 
 diff txt/albums.txt  txt/s_albums.txt  
 diff txt/artists.txt  txt/s_artists.txt  
--- scripts/test4.sh
+++ scripts/test4.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 rm txt/*.txt
 rm data/*.dat
-bin/s_rtags type tracks scan /media/seagate500GB/music/collection/flacs.cds /media/seagate500GB/music/collection/mp3s.only save
-bin/rtags load test
-bin/rtags load dump > txt/dump.txt
+time bin/s_rtags type tracks scan /media/seagate500GB/music/collection/flacs.cds /media/seagate500GB/music/collection/mp3s.only save
+time bin/rtags load test
+time bin/rtags load dump > txt/dump.txt
 
-bin/s_rtags type songs scan /media/seagate500GB/music/collection/flacs.cds /media/seagate500GB/music/collection/mp3s.only save
-bin/s_rtags type songs load test
-bin/s_rtags type songs load dump > txt/s_dump.txt
+echo ""
+echo "========================================"
+echo ""
+echo ""
+
+time bin/s_rtags type songs scan /media/seagate500GB/music/collection/flacs.cds /media/seagate500GB/music/collection/mp3s.only save
+time bin/s_rtags type songs load test
+time bin/s_rtags type songs load dump > txt/s_dump.txt
 
 diff txt/albums.txt  txt/s_albums.txt  
 diff txt/artists.txt  txt/s_artists.txt  
 diff txt/genres.txt  txt/s_genres.txt  
 diff txt/titles.txt txt/s_titles.txt
+#diff txt/dump.txt txt/s_dump.txt
