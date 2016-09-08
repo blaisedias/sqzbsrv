311893a79ed8ba414ff8f855d958b034461c0e8b
blaisedias <blaised02@gmail.com>
2016-09-08 14:51:57 +0100

    tweak test scripts

--- scripts/test5.sh
+++ scripts/test5.sh
@@ -3,18 +3,6 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 source $SCRIPT_DIR/medialocations
 mkdir -p txt
 mkdir -p data
-rm txt/*.txt >& /dev/null
-rm data/*.dat >& /dev/null
-
-echo "=tracks_db=s_rtags=create=rtags=load=test=dump==="
-echo ""
-bin/s_rtags type tracks scan $media_locations save
-bin/rtags load $media_locations test
-bin/rtags load $media_locations dump > txt/dump.txt
-
-echo ""
-echo ""
-echo ""
 echo "=songs_db=s_rtags=create=load=test=dump========="
 echo ""
 bin/s_rtags type songs scan $media_locations save
@@ -25,4 +13,3 @@ diff txt/albums.txt  txt/s_albums.txt
 diff txt/artists.txt  txt/s_artists.txt  
 diff txt/genres.txt  txt/s_genres.txt  
 diff txt/titles.txt txt/s_titles.txt
-#diff txt/dump.txt txt/s_dump.txt
